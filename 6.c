ef043ffd462574af29f815548235dd35b0706a1b
pawani sanghi <pawanisanghi17@gmail.com>
2025-09-08 23:05:09 +0530

    heyy

---
+++ 6.c
@@ -0,0 +1,129 @@
+#include<stdio.h>
+int main()
+{
+    // for(int i=0;i<3;i++)
+    // {
+    //     for (int j = 0; j<3 ; j++)
+    //     {
+    //         printf("* ");
+    //     }
+    //  printf(" \n");   
+    // }
+
+                               // output
+                               //  * * *  
+                               //  * * *  
+                               //  * * * 
+
+
+
+    // for(int i=0;i<=4;i++)
+    // {
+    //     for (int j = 0; j<=i ; j++)
+    //     {
+    //         printf("* ");
+    //     }
+    //  printf(" \n");   
+    // } 
+    
+    //                            output
+    //                            *          
+    //                            * *        
+    //                            * * *      
+    //                            * * * *    
+    //                            * * * * * 
+
+
+
+    // for(int i=0;i<=5;i++)
+    // {
+    //     for (int j=4 ; j>=i ; j--)
+    //     {
+    //         printf("   ");
+    //     }
+    //     for (int j =0; j<=i; j++)
+    //     {
+    //          printf(" * ");
+    //     }
+    //       printf("\n");
+    // } 
+
+            //           output 
+            //                   * 
+            //                *  *
+            //             *  *  *
+            //          *  *  *  *
+            //       *  *  *  *  *
+            //    *  *  *  *  *  *
+
+
+    // for(int i=0 ; i<5; i++)
+    // {
+    //     for (int j=4; j>i; j--)
+    //     {
+    //         printf(" * ");
+    //     }
+    //  printf(" \n");   
+    // } 
+
+    //               output 
+    //             *  *  *  *  
+    //             *  *  *
+    //             *  *
+    //             *
+
+
+
+    // for (int i = 0; i < 5; i++) 
+    // {
+    //     for (int j = 0; j < i; j++) 
+    //     {
+    //         printf("  "); 
+    //     }
+    //     for (int k = 0; k < 5 - i; k++) 
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n"); 
+    // }
+
+    //                output 
+    //                * * * * * 
+    //                  * * * *
+    //                    * * *
+    //                      * *
+    //                        *
+
+
+
+     for(int i=0;i<=5;i++)
+    {
+        for (int j=4 ; j>=i ; j--)
+        {
+            printf("   ");
+        }
+        for (int j =0; j<=i; j++)
+        {
+             printf(" * ");
+        }
+         for (int j = 1; j<=i ; j++)
+         {
+              printf(" * ");
+         }
+          printf("\n");
+    } 
+  
+    //                     output
+    //                       * 
+    //                    *  *  *
+    //                 *  *  *  *  *
+    //              *  *  *  *  *  *  *
+    //           *  *  *  *  *  *  *  *  *
+    //        *  *  *  *  *  *  *  *  *  *  *
+
+
+
+
+
+  return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+
+
+
+    for(int i=0;i<=4;i++)
+    {
+        for (int j = 0; j<=i ; j++)
+        {
