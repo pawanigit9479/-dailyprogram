d4df0f275eac95292627acb0cd7f80947e0b9443
pawani sanghi <pawanisanghi17@gmail.com>
2025-09-15 21:48:32 +0530

    hi

---
+++ 10.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main()
+{
+    // int a=100;
+    // int b=101;
+    // int arr[5]={1};
+    // int max=arr[0];
+    // for(int i=0;i<4;i++)
+    // {
+    //    if(arr[i]>max)
+    //     {
+    //         max=arr[i];
+    //     }
+    // }
+    // printf("%d",max);
+    // printf("%d",arr[45]);
+    // for(int i=2;i>=0;i--)
+    // {
+    //     printf("%d",arr[i]);
+    // }
+
+
+
+//   int arr[5]={3,12,52,34,5};
+//   int target=3;
+//   for(int i =0;i<5;i++)
+//   {
+//     if(arr[i]==target)
+//     {
+//         printf("%d",arr[i]);
+//         break;
+//     }
+//   }
+   
+
+
+    int a=101;
+    int b=102;
+    int c=103
+    int arr[5]={1,1,3,4,5};
+    int sum=0;
+    for(int i =0;i<5;i++)
+    {
+        sum= sum+arr[i];
+    }
+    for(int i=4;i>=0;i--)
+    {
+        printf("%d",arr[i]);
+    }
+
+    printf("%d",arr[1]);
+
+  return 0;
+}
