ff86a3a47220ad76c57063a835ce5bbf7da6ba9c
pawani sanghi <pawanisanghi17@gmail.com>
2025-10-05 23:29:25 +0530

    hi

--- lect17.c
+++ lect17.c
@@ -18,50 +18,50 @@ int main()
 
 
 
-    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int value = 1;
-    int t = 0, b = 2;
-    int left = 0, right = 2;
+    // int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    // int value = 1;
+    // int t = 0, b = 2;
+    // int left = 0, right = 2;
 
-    while (t <= b && left <= right)
-    {
-        for (int i = left; i <= right; i++)
-        {
-            arr[t][i] = value++;
-        }
-        t++;
-        for (int i = t; i <= b; i++) 
-        {
-            arr[i][right] = value++;
-        }
-        right--;
-        if (t <= b)
-        {
-            for (int i = right; i >= left; i--)
-            {
-                arr[b][i] = value++;
-            }
-            b--;
-        }
-        if (left <= right) 
-        {
-            for (int i = b; i >= t; i--) 
-            {
-                arr[i][left] = value++;
-            }
-            left++;
-        }
-    }
-    printf("Spiral Pattern:\n");
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-         {
-            printf("%2d ", arr[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+    // while (t <= b && left <= right)
+    // {
+    //     for (int i = left; i <= right; i++)
+    //     {
+    //         arr[t][i] = value++;
+    //     }
+    //     t++;
+    //     for (int i = t; i <= b; i++) 
+    //     {
+    //         arr[i][right] = value++;
+    //     }
+    //     right--;
+    //     if (t <= b)
+    //     {
+    //         for (int i = right; i >= left; i--)
+    //         {
+    //             arr[b][i] = value++;
+    //         }
+    //         b--;
+    //     }
+    //     if (left <= right) 
+    //     {
+    //         for (int i = b; i >= t; i--) 
+    //         {
+    //             arr[i][left] = value++;
+    //         }
+    //         left++;
+    //     }
+    // }
+    // printf("Spiral Pattern:\n");
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //      {
+    //         printf("%2d ", arr[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // return 0;
 
                 //   output: Spiral Pattern:
                 //           1  2  3       
@@ -70,8 +70,30 @@ int main()
 
 
 
-            // to print Z
-             
+int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+ for (int i = 0; i <3; i++)
+ {
+    for (int j = 0; j < 3; j++)
+    {
+        if (i==0 || i==2)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        else if (j==2-i-1)
+        {
+           printf("  %d",arr[i][j]);
+        }
+        else
+        {
+            printf("  ");
+        }
+    }
+    printf("\n");
+ }           
 }
+  
+                            // output: 1 2 3 
+                            //           4
+                            //         7 8 9
 
                     
